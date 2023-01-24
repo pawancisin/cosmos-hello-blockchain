dcbf6a91d107484cae30d6ece00c5f59294977aa
pawancisin <pawan.t@cisinlabs.com>
2023-01-24 18:48:23 +0530

    add query message api response

--- x/hello/keeper/grpc_query_hello.go
+++ x/hello/keeper/grpc_query_hello.go
@@ -3,21 +3,18 @@ package keeper
 import (
 	"context"
 
+	"hello/x/hello/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"hello/x/hello/types"
 )
 
 func (k Keeper) Hello(goCtx context.Context, req *types.QueryHelloRequest) (*types.QueryHelloResponse, error) {
-	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "invalid request")
-	}
-
-	ctx := sdk.UnwrapSDKContext(goCtx)
-
-	// TODO: Process the query
-	_ = ctx
-
-	return &types.QueryHelloResponse{}, nil
+    if req == nil {
+        return nil, status.Error(codes.InvalidArgument, "invalid request")
+    }
+    ctx := sdk.UnwrapSDKContext(goCtx)
+    _ = ctx
+    return &types.QueryHelloResponse{Text: "Hello, Ignite CLI!"}, nil // <--
 }
